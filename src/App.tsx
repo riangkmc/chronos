2ec4904a76f0ee6f0fb3d303b00bf460e1d508be
Rian Barbosa <riaann.barbosa@gmail.com>
2025-08-09 18:35:19 -0300

    adicionando logo e menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
+import { Container } from './components/Container/index.tsx'
+import { Heading } from './components/Heading/index.tsx'
+import { Logo } from './components/Logo/index.tsx'
+import { Menu } from './components/Menu/index.tsx'
+
 import './styles/theme.css'
 import './styles/App.css'
-import { Heading } from './components/Heading'
+
 
 export function App(){
     return(
         <>
-           <Heading/>
-           <p>Eu sou um elemento do app</p>
+        <Container>
+                <Heading>
+                    <Logo></Logo>
+                </Heading>
+        </Container>
+
+         <Container>
+                <Heading>
+                    <Menu></Menu>
+                </Heading>
+        </Container>
         </>
     )
 }
---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,12 @@
+import styles from './Heading.module.css'
+
+type HeadingProps = {
+    children:React.ReactNode;
+}
+
+export function Heading({children}:HeadingProps){
+    return(
+        <h1 className={`${styles.purple} ${styles.heading}`}>{children}</h1>
+        
+    )
+}
